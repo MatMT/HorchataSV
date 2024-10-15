ffa4ada5841805aecfef0314c80f16ee0f766f36
EliDev <oscarmateoelias@gmail.com>
2024-10-15 01:25:38 -0600

    refactor: DNS WWW

--- DNS-Service/configurar-zona-directa-ipv4.ps1
+++ DNS-Service/configurar-zona-directa-ipv4.ps1
@@ -4,7 +4,7 @@
 
 # Agregar registros A (IPv4)
 dnscmd /RecordAdd horchata.sv servidor A 172.16.0.2
-dnscmd /RecordAdd horchata.sv servicio-web A 172.16.0.2
+dnscmd /RecordAdd horchata.sv www A 172.16.0.2
 dnscmd /RecordAdd horchata.sv anfitrion A 172.16.0.4
 dnscmd /RecordAdd horchata.sv pineda A 172.16.0.21
 dnscmd /RecordAdd horchata.sv martin A 172.16.0.22
--- DNS-Service/configurar-zona-directa-ipv6.ps1
+++ DNS-Service/configurar-zona-directa-ipv6.ps1
@@ -4,7 +4,7 @@
 
 # Agregar registros AAAA (IPv6)
 dnscmd /RecordAdd horchata.sv servidor AAAA 2001:DB8:DEA:B::2
-dnscmd /RecordAdd horchata.sv servicio-web AAAA 2001:DB8:DEA:B::2
+dnscmd /RecordAdd horchata.sv www AAAA 2001:DB8:DEA:B::2
 dnscmd /RecordAdd horchata.sv anfitrion AAAA 2001:DB8:DEA:B::4
 dnscmd /RecordAdd horchata.sv pineda AAAA 2001:DB8:DEA:B::21
 dnscmd /RecordAdd horchata.sv martin AAAA 2001:DB8:DEA:B::22
