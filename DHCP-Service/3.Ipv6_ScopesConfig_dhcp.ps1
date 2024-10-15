a0e45202f9e9b72f64588fdae16e4aa05c622a68
EliDev <oscarmateoelias@gmail.com>
2024-10-15 05:18:22 -0600

    feat: DNS & DHCPv6

--- DHCP-Service/3.Ipv6_ScopesConfig_dhcp.ps1
+++ DHCP-Service/3.Ipv6_ScopesConfig_dhcp.ps1
@@ -3,8 +3,10 @@
 #IPv6 Scope Configuration
 Add-DhcpServerv6Scope -Name "Scopev6" -Prefix 2001:db8:dea:b:: -State Active
 
-Set-DhcpServerv6OptionValue -Prefix 2001:db8:dea:b:: -DnsServer 2001:db8:dea:b::2
+# DNS Configuration
+Set-DhcpServerv6OptionValue -DnsServer 2001:db8:dea:b::2 
+# Set-DhcpServerv6OptionValue -Prefix 2001:db8:dea:b:: -DnsServer 2001:db8:dea:b::2
 
+# Range Exclussion
 Add-DhcpServerv6ExclusionRange -Prefix 2001:db8:dea:b:: -StartRange 2001:db8:dea:b::1 -EndRange 2001:db8:dea:b::99
-
 Add-DhcpServerv6ExclusionRange -Prefix 2001:db8:dea:b:: -StartRange 2001:db8:dea:b::201 -EndRange 2001:db8:dea:b:ffff:ffff:ffff:ffff
