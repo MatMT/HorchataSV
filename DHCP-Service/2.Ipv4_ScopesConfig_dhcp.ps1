dcda4f6dcb60163bc6e105ffb36114a3c0124d9a
EliDev <oscarmateoelias@gmail.com>
2024-10-15 01:34:24 -0600

    refactor: ScopedId

--- DHCP-Service/2.Ipv4_ScopesConfig_dhcp.ps1
+++ DHCP-Service/2.Ipv4_ScopesConfig_dhcp.ps1
@@ -3,4 +3,5 @@
 #IPv4 Scope Configuration
 Add-DhcpServerv4Scope -Name "Scopev4" -StartRange 172.16.0.50 -EndRange 172.16.0.100 -SubnetMask 255.255.255.0 -State Active
 
-Set-DhcpServerv4OptionValue -OptionId 6 -Value "172.16.0.2" -Scopeld "172.16.0.0"
+# DNS Configuration
+Set-DhcpServerv4OptionValue -OptionId 6 -Value "172.16.0.2" -ScopeId "172.16.0.0"
