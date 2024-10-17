e97a851f1bee40ae174c4f2be5279e156e95fffb
EliDev <82849501+MatMT@users.noreply.github.com>
2024-10-17 00:12:07 -0600

    Update configurar-zona-inversa-ipv6.ps1

--- DNS-Service/configurar-zona-inversa-ipv6.ps1
+++ DNS-Service/configurar-zona-inversa-ipv6.ps1
@@ -3,12 +3,12 @@
 # Configurar zona inversa IPv6
 
 # Crear la zona inversa para la red IPv6 2001:DB8:DEA:B::/64
-dnscmd /ZoneAdd B.EA.D.D.B.0.1.0.0.2.ip6.arpa /Primary
+dnscmd /ZoneAdd b.e.a.d.b.8.d.0.1.0.0.2.ip6.arpa /Primary
 
 # Agregar registros PTR para IPv6
-dnscmd /RecordAdd B.EA.D.D.B.0.1.0.0.2.ip6.arpa 2 PTR servidor.horchata.sv
-dnscmd /RecordAdd B.EA.D.D.B.0.1.0.0.2.ip6.arpa 4 PTR anfitrion.horchata.sv
-dnscmd /RecordAdd B.EA.D.D.B.0.1.0.0.2.ip6.arpa 21 PTR pineda.horchata.sv
-dnscmd /RecordAdd B.EA.D.D.B.0.1.0.0.2.ip6.arpa 22 PTR martin.horchata.sv
-dnscmd /RecordAdd B.EA.D.D.B.0.1.0.0.2.ip6.arpa 23 PTR javi.horchata.sv
-dnscmd /RecordAdd B.EA.D.D.B.0.1.0.0.2.ip6.arpa 24 PTR marcelo.horchata.sv
+dnscmd /RecordAdd b.e.a.d.b.8.d.0.1.0.0.2.ip6.arpa 2 PTR servidor.horchata.sv
+dnscmd /RecordAdd b.e.a.d.b.8.d.0.1.0.0.2.ip6.arpa 4 PTR anfitrion.horchata.sv
+dnscmd /RecordAdd b.e.a.d.b.8.d.0.1.0.0.2.ip6.arpa 21 PTR pineda.horchata.sv
+dnscmd /RecordAdd b.e.a.d.b.8.d.0.1.0.0.2.ip6.arpa 22 PTR martin.horchata.sv
+dnscmd /RecordAdd b.e.a.d.b.8.d.0.1.0.0.2.ip6.arpa 23 PTR javi.horchata.sv
+dnscmd /RecordAdd b.e.a.d.b.8.d.0.1.0.0.2.ip6.arpa 24 PTR marcelo.horchata.sv
